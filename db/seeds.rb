be72068d436168df2a942369de92aa7b8f92705f
Adam Butler <adam@lab.io>
2015-05-24 09:01:25 +0100

    Bring seeds up to date with all models and attribues

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,20 +1,44 @@
 # This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+# The data can then be loaded with the rake db:seed (or create!d alongside the db with db:setup).
 #
 # Examples:
 #
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+#   cities = City.create!([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create!(name: 'Emanuel', city: cities.first)
 
-group = Group.create({
+group = Group.create!({
   title: "Bristol JS"
 })
 
 events = []
 
 (1..10).each do |i|
-  events << Event.create({
+  events << Event.create!({
     title: "Talk Night ##{i}",
     group_id: group.id
   })
+end
+
+users = []
+
+(1..10).each do |i|
+  users << User.create!({
+    first_name: "Adam",
+    surname: "Butler",
+    twitter_url: "https://twitter.com/rails",
+    github_url: "http://github.com/rails",
+    dob: "01-01-2000",
+    email: "user#{i}@techtalks.io",
+    password: "itsasecret"
+  })
+end
+
+users.each do |user|
+  events.each do |event|
+    Rsvp.create!({
+      user: user,
+      event: event,
+      attending: [true, false, nil].sample
+    })
+  end
 end
