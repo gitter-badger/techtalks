8fdf76527512555c8a884183201319f35f9cf987
Adam Butler <adam@lab.io>
2015-05-24 09:00:55 +0100

    Create relationships to rsvp

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,7 @@
 class Event < ActiveRecord::Base
   belongs_to :group
   has_one :location
+  has_many :rsvps
 
   validates_presence_of :title, :group_id
 
--- app/models/rsvp.rb
+++ app/models/rsvp.rb
@@ -1,2 +1,5 @@
 class Rsvp < ActiveRecord::Base
+  validates_presence_of :event, :user
+  belongs_to :event
+  belongs_to :user
 end
